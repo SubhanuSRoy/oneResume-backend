e126de13ad2c2de59e1bf53e82451d4d86542bdb
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2023-08-27 21:55:18 +0530

    ft. added cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const indexRoutes = require('./routes/indexRoutes');
 const uploadRoutes = require('./routes/uploadRoutes');
+
+app.use(cors()); // Use the cors middleware
 app.use(express.json());
 
 app.use('/', indexRoutes);
