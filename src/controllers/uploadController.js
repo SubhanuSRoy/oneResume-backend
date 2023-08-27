0aa0b43c9c1359f87751c4460df2206827948f44
Subhanu Sankar Roy <71728589+SubhanuSRoy@users.noreply.github.com>
2023-08-27 20:31:33 +0530

    ft. updated with working public limk

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,31 +1,51 @@
 // src/controllers/uploadController.js
-const storage = require('../firebase');
+const storage = require("../firebase");
+const { getStorage, getDownloadURL } = require('firebase-admin/storage');
+
 
 const uploadFile = async (file, user_id) => {
-    try {
-        const bucket = storage.bucket();
-        const fileBlob = bucket.file(`resumes/${user_id}_resume.pdf`);
-        const blobStream = fileBlob.createWriteStream();
+  try {
+    const bucket = storage.bucket();
+    const fileBlob = bucket.file(`resumes/${user_id}_resume.pdf`);
+    const blobStream = fileBlob.createWriteStream();
 
-        return new Promise((resolve, reject) => {
-            blobStream.on('error', (error) => {
-                console.error(error);
-                reject('Upload failed');
-            });
-
-            blobStream.on('finish', () => {
-                const publicUrl = `https://storage.googleapis.com/${bucket.name}/${fileBlob.name}`;
-                resolve(publicUrl);
-            });
-
-            blobStream.end(file.buffer);
-        });
-    } catch (error) {
+    return new Promise((resolve, reject) => {
+      blobStream.on("error", (error) => {
         console.error(error);
-        throw new Error('Internal server error');
-    }
+        reject("Upload failed");
+      });
+
+      
+      blobStream.on("finish", () => {
+        
+        const fileRef = getStorage().bucket(bucket.name).file(fileBlob.name);
+        const downloadURL= getDownloadURL(fileRef);
+        // fileBlob
+        //   .getMetadata()
+        //   .then((metadata) => {
+        //     const downloadUrl = metadata[0].mediaLink;
+
+        //     // The download URL can be used to access the file
+        //     console.log(downloadUrl);
+        //     resolve(downloadUrl);
+        //   })
+        //   .catch((error) => {
+        //     // Handle any errors
+        //     console.error(error);
+        //   });
+        // const publicUrl = `https://firebasestorage.googleapis.com/${bucket.name}/${fileBlob.name}?alt=media`;
+        // resolve(publicUrl);
+        resolve(downloadURL);
+      });
+
+      blobStream.end(file.buffer);
+    });
+  } catch (error) {
+    console.error(error);
+    throw new Error("Internal server error");
+  }
 };
 
 module.exports = {
-    uploadFile,
+  uploadFile,
 };
